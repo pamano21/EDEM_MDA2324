dc99ab24f41d65ccf5259e9744612c7bce8c9164
pamano21 <145840762+pamano21@users.noreply.github.com>
2023-12-02 10:30:03 +0100

    gg

--- Alumnos/FS/Pablo Martí Nomdedeu/ABUELOS/discapacitados.py
+++ Alumnos/FS/Pablo Martí Nomdedeu/ABUELOS/discapacitados.py
@@ -1,10 +1,9 @@
-import pandas as pd 
-#eee
 
+import pandas as pd 
 # Cargar el archivo CSV en un DataFrame (reemplaza 'usuarios.csv' con el nombre de tu archivo)
 
 nombre_archivo = 'usuarios.csv'
-data = pd.read_csv("usuarios.csv")
+data = pd.read_csv("/content/usuarios.csv")
 
 # Verificar si 'discapacidad' es una columna en el archivo
 if 'discapacidad' in data.columns:
@@ -12,7 +11,7 @@ if 'discapacidad' in data.columns:
     data['puntos'] = data['discapacidad']  # Se asume que los valores ya están en el rango de 0% a 100%
 
     # Ordenar los usuarios en función de los puntos de discapacidad en orden ascendente
-    usuarios_ordenados_discapacidad = data.sort_values(by='puntos', ascending = False)
+    usuarios_ordenados_discapacidad = data.sort_values(by='puntos', ascending=True)
 
     # Imprimir los usuarios en orden ascendente según los puntos obtenidos por discapacidad
     print("Usuarios en orden ascendente según los puntos de discapacidad:")
